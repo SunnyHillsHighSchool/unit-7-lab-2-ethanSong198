070702218dd3410237cfd204ed63decc4358190c
Ethan Song <600020389@fjuhsd.org>
2021-02-02 16:50:24 +0000

    Created method

--- ListLast.java
+++ ListLast.java
@@ -10,6 +10,18 @@ public class ListLast
 {
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+    int last = ray.get(ray.size() - 1);
+
+    //create for loop with int x of ray
+    for(int x = 0; x < ray.size() - 1; x++){
+
+      //create if loop to compare the last number to current number in arraylist
+      if(ray.get(x) == last){
+        //return true
+        return true;
+      }
+    }
+    //return false
+    return false;
 	}
 }
